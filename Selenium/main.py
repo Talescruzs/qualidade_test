66b91fb2152c455746c301ae3b9811624707ea5c
Talescruzs <talescruzdasilva@gmail.com>
2025-06-03 11:13:04 -0300

    vai ser com python mesmo, mais facil

---
+++ Selenium/main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+
+
+
+# Create a new instance of the Chrome driver
+
+driver = webdriver.Firefox()
+driver.get('http://google.com')
+print(driver.title)
+driver.quit()

71bd7442e5ec0b63ea9387a293a0e7c70992c222
Talescruzs <talescruzdasilva@gmail.com>
2025-06-03 17:22:57 -0300

    testando coisas

--- Selenium/seleniumDrive.py
+++ Selenium/seleniumDrive.py
@@ -8,6 +8,16 @@ class TestUniversity():
 
     def setup_method(self):
         self.driver.get('https://www.uni-muenchen.de/index.html')
+    
+    def get_main_menu(self):
+        main_menu = self.driver.find_element("class name", "main-navigation__list-link")
+        return main_menu
+    
+    def click(self, element):
+        try:
+            element.click()
+        except Exception as e:
+            print(f"Error clicking element: {e}")
         
 if __name__ == "__main__":
     teste = TestUniversity()
--- Selenium/tests.py
+++ Selenium/tests.py
@@ -2,7 +2,6 @@ import pytest
 from seleniumDrive import TestUniversity
 
 class TestSeleniumDrive:
-    @pytest.fixture(autouse=True)
     def setup(self):
         self.selenium_instance = TestUniversity()
         self.mainLink = "https://www.uni-muenchen.de/index.html"
@@ -10,6 +9,14 @@ class TestSeleniumDrive:
     def test_driver_initialization(self):
         assert self.selenium_instance.driver is not None, "Driver is not initialized"
 
-    def test_google_page_load(self):
+    # Testes com selenium 
+    def test_page_title(self):
         self.selenium_instance.setup_method()
         assert self.selenium_instance.driver.title == "Startseite - LMU München" , "Erro ao chegar no site"
+
+    def test_main_menu(self):
+        main_menu = self.selenium_instance.get_main_menu()
+        assert main_menu is not None, "Main menu not found"
+        assert self.selenium_instance.click(main_menu) is None, "Error clicking main menu"
+
+
